b929e0f4e66be7200119b27e401b4fdc7a046ee1
shenjialong <shenjialong@autohome.com.cn>
2021-04-07 23:24:42 +0800

    20210407-v2 1、公告变更-查重服务

--- src/main/java/com/gto/bang/controller/BaseInfoController.java
+++ src/main/java/com/gto/bang/controller/BaseInfoController.java
@@ -1,9 +1,13 @@
 package com.gto.bang.controller;
 
 import com.gto.bang.common.net.Response;
+import com.gto.bang.model.Operation;
+import com.gto.bang.service.LogService;
 import org.apache.commons.collections.map.HashedMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -20,6 +25,20 @@ import java.util.Map;
 public class BaseInfoController extends BaseController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BaseInfoController.class);
+
+    @Autowired
+    LogService logService;
+
+    @Value("${baseinfo.wechat}")
+    String wechat;
+
+    @RequestMapping(value = "/base/info")
+    @ResponseBody
+    public Map<String, Object> list(Integer courseId) throws IOException {
+        Map<String, String> data = new HashedMap();
+        data.put("wechat", wechat);
+        return supports(data);
+    }
 
     /**
      * @param userId
